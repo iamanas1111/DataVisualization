abaf8a206d0f66ea6fba367443226cca0e543fed
Mohd Anas <anas@botlie.com>
2023-06-26 11:53:25 +0530

    add

--- src/main/java/com/Visualization/repository/VisualizationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Visualization.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Visualization.entity.VisualizationEntity;
-
-public interface VisualizationRepository extends JpaRepository<VisualizationEntity,Integer>{
-
-}
